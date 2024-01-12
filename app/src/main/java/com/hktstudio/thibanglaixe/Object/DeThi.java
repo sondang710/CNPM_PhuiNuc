87618e401874b03e7010a92d44d521269aec7857
SonCaiBe <soncaibe007@gmail.com>
2024-01-12 15:38:23 +0700

    aa

---
+++ app/src/main/java/com/hktstudio/thibanglaixe/Object/DeThi.java
@@ -0,0 +1,22 @@
+package com.hktstudio.thibanglaixe.Object;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+
+public class DeThi implements Serializable{
+    List<CauHoi> listCauHoi;
+
+    public DeThi(List<CauHoi> listCauHoi) {
+        this.listCauHoi = listCauHoi;
+    }
+
+    public List<CauHoi> getListCauHoi() {
+        return listCauHoi;
+    }
+
+    public void setListCauHoi(List<CauHoi> listCauHoi) {
+        this.listCauHoi = listCauHoi;
+    }
+}
